6498c3585a3687ac1d1d59ce5c5c124756d497c8
empitsu0813 <oneok0813@gmail.com>
2019-12-27 10:23:04 +0900

    create header

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-purple w-full px-2">
+    <nav class="flex justify-between p-8 items-center mb-16">
+      <a
+        href="#"
+        class="font-sans text-black md:text-white text-sm md:text-4xl font-thin"
+        >Massive Dynamics</a
+      >
+      <ul class="list-reset flex">
+        <li>
+          <a href="#" class="text-black md:text-white p-2 md:p-4">Home</a>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="text-black md:text-white p-2 md:p-4 whitespace-no-wrap"
+            >About Us</a
+          >
+        </li>
+        <li>
+          <a href="#" class="text-black md:text-white p-2 md:p-4">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
